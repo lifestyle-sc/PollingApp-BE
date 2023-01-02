187b0ac2253e6586f41a3a03bbb62ccb0b802caf
Oluwakayode Momoh <68966387+lifestyle-sc@users.noreply.github.com>
2023-01-02 17:20:53 +0100

    created context class which inherit entity framework db context and also added the connection string

---
+++ Repository/RepositoryContext.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<User>? Users { get; set; }
+        public DbSet<Poll>? Polls { get; set; }
+        public DbSet<Candidate> Candidates { get; set; }
+    }
+}

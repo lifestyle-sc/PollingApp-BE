7d9b299f053849b5496a383a4e5205d2b7fe4695
Oluwakayode Momoh <68966387+lifestyle-sc@users.noreply.github.com>
2023-01-02 11:32:49 +0100

    Installed Nlog library and implemented ILoggerManager interface in LoggerService project

---
+++ LoggerService/LoggerManager.cs
@@ -0,0 +1,23 @@
+﻿using Contracts;
+using NLog;
+using System.Security.Cryptography;
+
+namespace LoggerService
+{
+    public class LoggerManager : ILoggerManager
+    {
+        private static ILogger logger = LogManager.GetCurrentClassLogger();
+
+        public void LogInfo(string message) =>
+            logger.Info(message);
+
+        public void LogWarn(string message) =>
+            logger.Warn(message);
+
+        public void LogDebug(string message) =>
+            logger.Debug(message);
+
+        public void LogError(string message) => 
+            logger.Error(message);
+    }
+}

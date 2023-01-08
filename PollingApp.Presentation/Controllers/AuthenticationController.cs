b3616e96c86794f6051038828cd9f7b5f5778f5e
Oluwakayode Momoh <68966387+lifestyle-sc@users.noreply.github.com>
2023-01-08 17:46:58 +0100

    created auth controller in the presentation layer.

---
+++ PollingApp.Presentation/Controllers/AuthenticationController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
+
+namespace PollingApp.Presentation.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IServiceManager _services;
+        public AuthenticationController(IServiceManager services) => _services = services;
+    }
+}

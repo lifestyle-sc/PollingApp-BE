1755b649e4c20f3126f4a31697da938279bbac6d
Oluwakayode Momoh <68966387+lifestyle-sc@users.noreply.github.com>
2023-01-10 04:27:19 +0100

    Created new roles using identityrole in role configuration and migrated it to database

--- Repository/RepositoryContext.cs
+++ Repository/RepositoryContext.cs
@@ -1,6 +1,7 @@
 ﻿using Entities.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Repository.Configuration;
 
 namespace Repository
 {
@@ -14,6 +15,8 @@ namespace Repository
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
+            builder.ApplyConfiguration(new RoleConfiguration());
         }
 
         public DbSet<Poll> Polls { get; set; }

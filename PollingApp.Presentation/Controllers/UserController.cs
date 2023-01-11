60100382439f39e3df68e24ac7061fb2d46336e0
Oluwakayode Momoh <68966387+lifestyle-sc@users.noreply.github.com>
2023-01-11 22:58:21 +0100

    created user controller and implemented get user endpoint

---
+++ PollingApp.Presentation/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
+using Shared.DTOs;
+
+namespace PollingApp.Presentation.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IServiceManager _services;
+
+        public UserController(IServiceManager services) => _services = services;
+
+        [HttpGet("{id:guid}")]
+        public async Task<IActionResult> GetUser(Guid id)
+        {
+            var user = await _services.UserService.GetUserAsync(id);
+
+            return Ok(user);
+        }
+    }
+}

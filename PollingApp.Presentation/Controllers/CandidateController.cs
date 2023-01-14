6884f71e704af1b59b83fba0735af100e375935a
Oluwakayode Momoh <68966387+lifestyle-sc@users.noreply.github.com>
2023-01-14 15:51:20 +0100

    created candidate controller in the presentation layer

---
+++ PollingApp.Presentation/Controllers/CandidateController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
+
+namespace PollingApp.Presentation.Controllers
+{
+    [Route("api/users/{userId}/polls/{pollId}/candidates")]
+    [ApiController]
+    public class CandidateController : ControllerBase
+    {
+        private readonly IServiceManager _services;
+
+        public CandidateController(IServiceManager services) => _services = services;
+    }
+}

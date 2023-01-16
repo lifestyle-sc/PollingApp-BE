ee19a9431451e1f221a3d6681734adece055f77b
Oluwakayode Momoh <68966387+lifestyle-sc@users.noreply.github.com>
2023-01-16 13:30:03 +0100

    created candidate not found exception class

---
+++ Entities/Exceptions/CandidateNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace Entities.Exceptions
+{
+    public sealed class CandidateNotFoundException : NotFoundException
+    {
+        public CandidateNotFoundException(Guid id) : base($"The candidate with Id: {id} doesn't exist in the database.")
+        {
+
+        }
+    }
+}
--- Service.Contracts/ICandidateService.cs
+++ Service.Contracts/ICandidateService.cs
@@ -5,5 +5,6 @@ namespace Service.Contracts
     public interface ICandidateService
     {
         Task<CandidateDto> CreateCandidateForPollAsync(Guid userId, Guid pollId, CandidateForCreationDto candidateForCreation, bool trackChanges);
+        Task<CandidateDto> GetCandidateForPollAsync(Guid userId, Guid pollId, Guid id, bool trackChanges);
     }
 }

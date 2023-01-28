235becac6e7f6e834b4bffcf2749f41fadd3a40b
Oluwakayode Momoh <68966387+lifestyle-sc@users.noreply.github.com>
2023-01-28 16:41:24 +0100

    added orderby property to request paramter

--- Shared/RequestFeatures/CandidateParameters.cs
+++ Shared/RequestFeatures/CandidateParameters.cs
@@ -2,6 +2,7 @@
 {
     public class CandidateParameters : RequestParameters
     {
+        public CandidateParameters() => OrderBy = "name";
         public string? SearchTerm { get; set; }
     }
 }

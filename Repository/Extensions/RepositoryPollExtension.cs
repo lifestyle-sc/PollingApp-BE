fba58bff7b0c89690b14f31d9b167fefed9f00f9
Oluwakayode Momoh <68966387+lifestyle-sc@users.noreply.github.com>
2023-01-28 17:39:04 +0100

    changed IsNullOrEmpty to IsNullOrWhiteSpace in the search extension method for poll repository

--- Repository/Extensions/RepositoryPollExtension.cs
+++ Repository/Extensions/RepositoryPollExtension.cs
@@ -6,12 +6,14 @@ namespace Repository.Extensions
     {
         public static IQueryable<Poll> Search(this IQueryable<Poll> polls, string searchTerm)
         {
-            if (String.IsNullOrEmpty(searchTerm))
+            if (String.IsNullOrWhiteSpace(searchTerm))
                 return polls;
 
             var lowerCaseTerm = searchTerm.Trim().ToLower();
 
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
             return polls.Where(x => x.Name.ToLower().Contains(searchTerm));
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
         }
     }
 }

c2a4b265fa7b9c09d8f6f072485be4480d335539
Oluwakayode Momoh <68966387+lifestyle-sc@users.noreply.github.com>
2023-03-26 23:54:21 +0100

    added authoriaztion to swagger

--- PollingApp/Extensions/ServiceExtensions.cs
+++ PollingApp/Extensions/ServiceExtensions.cs
@@ -134,6 +134,30 @@ namespace PollingApp.Extensions
             services.AddSwaggerGen(s =>
             {
                 s.SwaggerDoc("v1", new OpenApiInfo { Title = "Polling App API", Version = "v1" });
+
+                s.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Place to add JWT with Bearer",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                s.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference{
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Name = "Bearer",
+                        },
+                        new List<string>()
+                    }
+                });
             });
         }
 
